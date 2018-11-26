6e573c332a03c2beaa373a968caa62a450358690
rovast <rovast@163.com>
2018-11-26 21:21:46 +0800

    🎨 完成两个学生手抄试卷的写法

---
+++ src/Chapter10/v1/TestPaperA.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rovast\DesignPatternTalk\Chapter10\v1;
+
+/**
+ * Class TestPaperA
+ *
+ * @package \Rovast\DesignPatternTalk\Chapter10\v1
+ */
+class TestPaperA
+{
+    public function question1()
+    {
+        echo 'Q1: 中国首都是哪？ 答：北京' . PHP_EOL;
+    }
+
+    public function question2()
+    {
+        echo 'Q2: 印度的首都是啥？ 答：新德里' . PHP_EOL;
+    }
+
+    public function question3()
+    {
+        echo 'Q3: 美国的首都是啥？ 答：华盛顿' .PHP_EOL;
+    }
+}
