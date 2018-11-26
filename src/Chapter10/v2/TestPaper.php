9df6aeb63766e878bf92a5fb6b4f01ff7358d93f
rovast <rovast@163.com>
2018-11-26 21:26:17 +0800

    🎨 两个人抄一份题目的实现

---
+++ src/Chapter10/v2/TestPaper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rovast\DesignPatternTalk\Chapter10\v2;
+
+/**
+ * Class TestPaper
+ *
+ * @package \Rovast\DesignPatternTalk\Chapter10\v2
+ */
+class TestPaper
+{
+    public function question1()
+    {
+        echo 'Q1: 中国首都是哪？' . PHP_EOL;
+    }
+
+    public function question2()
+    {
+        echo 'Q2: 印度的首都是啥？' . PHP_EOL;
+    }
+
+    public function question3()
+    {
+        echo 'Q3: 美国的首都是啥？' .PHP_EOL;
+    }
+}
---
+++ src/Chapter10/v2/TestPaperB.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rovast\DesignPatternTalk\Chapter10\v2;
+
+/**
+ * Class TestPaperB
+ *
+ * @package \Rovast\DesignPatternTalk\Chapter10\v2
+ */
+class TestPaperB extends TestPaper
+{
+    public function question1()
+    {
+        parent::question1();
+        echo ' 答：北京' . PHP_EOL;
+    }
+
+    public function question2()
+    {
+        parent::question2();
+        echo '答：新德里' . PHP_EOL;
+    }
+
+    public function question3()
+    {
+        parent::question3();
+        echo '答：华盛顿' .PHP_EOL;
+    }
+}
