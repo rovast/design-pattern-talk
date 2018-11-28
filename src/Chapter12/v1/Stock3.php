01232ff92e9380089178840be96f9f0a57ecec50
ROVAST <rovast@163.com>
2018-11-28 13:11:39 +0000

    Apply fixes from StyleCI

--- src/Chapter12/v1/Stock3.php
+++ src/Chapter12/v1/Stock3.php
@@ -3,19 +3,17 @@
 namespace Rovast\DesignPatternTalk\Chapter12\v1;
 
 /**
- * Class Stock3
- *
- * @package \Rovast\DesignPatternTalk\Chapter12\v1
+ * Class Stock3.
  */
 class Stock3
 {
     public function sell()
     {
-        echo '股票3 卖出' . PHP_EOL;
+        echo '股票3 卖出'.PHP_EOL;
     }
 
     public function buy()
     {
-        echo '股票3 买入' . PHP_EOL;
+        echo '股票3 买入'.PHP_EOL;
     }
 }
--- src/Chapter12/v2/Realty1.php
+++ src/Chapter12/v2/Realty1.php
@@ -3,19 +3,17 @@
 namespace Rovast\DesignPatternTalk\Chapter12\v2;
 
 /**
- * Class Realty1
- *
- * @package \Rovast\DesignPatternTalk\Chapter12\v2
+ * Class Realty1.
  */
 class Realty1
 {
     public function sell()
     {
-        echo '房地产1 卖出' . PHP_EOL;
+        echo '房地产1 卖出'.PHP_EOL;
     }
 
     public function buy()
     {
-        echo '房地产1 买入' . PHP_EOL;
+        echo '房地产1 买入'.PHP_EOL;
     }
 }
