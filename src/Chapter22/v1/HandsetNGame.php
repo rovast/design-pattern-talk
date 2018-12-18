2a62a08a08f2680db339f9fdd54ac1f7f7c957b7
rovast <rovast@163.com>
2018-12-18 21:52:03 +0800

    ✨ 完成N手机游戏运行

---
+++ src/Chapter22/v1/HandsetNGame.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Rovast\DesignPatternTalk\Chapter22\v1;
+
+/**
+ * Class HandsetNGame
+ *
+ * @package \Rovast\DesignPatternTalk\Chapter22\v1
+ */
+class HandsetNGame
+{
+    public function run()
+    {
+        echo '运行 N 品牌游戏' . PHP_EOL;
+    }
+}
