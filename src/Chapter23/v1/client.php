ce00ac64a9a75d26a2041e8a34607ec0d61da38d
rovast <rovast@163.com>
2018-12-19 22:24:33 +0800

    ✨ 路边烧烤摊的烧烤模式

---
+++ src/Chapter23/v1/client.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__.'/../../../vendor/autoload.php';
+
+$boy = new \Rovast\DesignPatternTalk\Chapter23\v1\Barbecuer();
+
+$boy->BakeChickenWing();
+$boy->BakeMutton();
+$boy->BakeMutton();
+$boy->BakeMutton();
+$boy->BakeMutton();
+$boy->BakeMutton();
+$boy->BakeMutton();
