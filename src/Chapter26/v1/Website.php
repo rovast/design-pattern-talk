43bc6f75b59b65afe7b4601d208ac4d83921eba9
rovast <rovast@163.com>
2018-12-25 20:44:04 +0800

    ✨ 普通模式进行多个网站的新建

---
+++ src/Chapter26/v1/Website.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rovast
+ * Date: 2018-12-25
+ * Time: 20:37
+ */
+
+namespace Rovast\DesignPatternTalk\Chapter26\v1;
+
+class Website
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function use()
+    {
+        echo '网站分类：' . $this->name . PHP_EOL;
+    }
+}
---
+++ src/Chapter26/v1/client.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__.'/../../../vendor/autoload.php';
+
+$fx = new \Rovast\DesignPatternTalk\Chapter26\v1\Website('产品展示');
+$fx->use();
+
+$fy = new \Rovast\DesignPatternTalk\Chapter26\v1\Website('产品展示');
+$fy->use();
+
+$fz = new \Rovast\DesignPatternTalk\Chapter26\v1\Website('博客');
+$fz->use();
+
+$fl = new \Rovast\DesignPatternTalk\Chapter26\v1\Website('博客');
+$fl->use();
+
+$fm = new \Rovast\DesignPatternTalk\Chapter26\v1\Website('博客');
+$fm->use();
