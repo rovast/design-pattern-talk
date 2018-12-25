b0a95712d1ca4f2714d742e34e3a75f93a65ac2c
ROVAST <rovast@163.com>
2018-12-25 12:57:32 +0000

    Apply fixes from StyleCI

--- src/Chapter26/v1/Website.php
+++ src/Chapter26/v1/Website.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: rovast
  * Date: 2018-12-25
- * Time: 20:37
+ * Time: 20:37.
  */
 
 namespace Rovast\DesignPatternTalk\Chapter26\v1;
@@ -22,6 +22,6 @@ class Website
 
     public function use()
     {
-        echo '网站分类：' . $this->name . PHP_EOL;
+        echo '网站分类：'.$this->name.PHP_EOL;
     }
 }
